c015aa94a3bd9f9ff21ababf69510c6c0744adff
Dogukan Kaska <dogukankaska@192.168.1.9>
2023-11-22 19:49:01 +0300

    secondCommit

--- ceng105_github_assignment/main.c
+++ ceng105_github_assignment/main.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    
+
     int grade;
 
     printf("Enter your final grade (between 0 and 100): ");
     scanf("%d", &grade);
 
-    if (grade < 45) {
-        printf("You failed the course\n");
+    if (grade >= 90 && grade <= 100) {
+        printf("Your grade is A\n");
+    } else if (grade >= 75 && grade <= 89) {
+        printf("Your grade is B\n");
+    } else if (grade >= 55 && grade <= 74) {
+        printf("Your grade is C\n");
+    } else if (grade >= 45 && grade <= 54) {
+        printf("Your grade is D\n");
+    } else if (grade >= 0 && grade <= 44) {
+        printf("Your grade is F\n");
     } else {
-        printf("You passed the course\n");
+        printf("Invalid grade entered. Please enter a grade between 0 and 100.\n");
     }
 
     return 0;
